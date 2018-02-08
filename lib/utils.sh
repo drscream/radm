234369ade47cd4bebb908f68e638dd08920a6775
Thomas Merkel <tm@core.io>
2018-02-08 14:25:46 +0100

    First initial commit to start working with radm

---
+++ lib/utils.sh
@@ -0,0 +1,20 @@
+# Script provide utils functions which may used by radm
+# vim:ft=sh
+
+render() {
+	local inFile="${1}"
+
+	while read -r line ; do
+		while [[ "${line}" =~ (\$\{[a-zA-Z_][a-zA-Z_0-9]*\}) ]] ; do
+			LHS=${BASH_REMATCH[1]}
+			RHS="$(eval echo "\"${LHS}\"")"
+			line=${line//$LHS/$RHS}
+		done
+		echo -e "${line}"
+	done < ${inFile}
+}
+
+err() {
+	local msg=${@}
+	echo Error: ${msg}
+}
---
+++ lib/vmadm.sh
@@ -0,0 +1,58 @@
+vmadm() {
+	case "${1}" in
+		create)
+			local templateFile="var/templates/${2}.json"
+			local tempFile=$(mktemp --suffix=radm)
+
+			# Verify template json file exists
+			if [[ ! -f ${templateFile} ]]; then
+				err "Template does not exists"
+				return 1
+			fi
+
+			# Define variables based on each parameter
+			for entry in ${@}; do
+				if [[ ${entry} = --* ]]; then
+					# Remove --
+					local entry=${entry##--}
+					# Split by equal
+					local variable=${entry%=*}
+					local value=${entry##*=}
+					# Defaults for DNS_DOMAIN
+					DNS_DOMAIN=${FQDN#*.}
+					# Provide VARIABLES
+					eval "${variable^^}=${value}"
+					# Network special for ips and gateways
+					case ${variable} in
+						ips)
+							local ips+=("${value}")
+						;;
+						gateways)
+							local gateways+=("${value}")
+						;;
+					esac
+					# Overwrite IPS and GATEWAYS
+					local tmpIps=$(printf ",\"%s\"" "${ips[@]}")
+					local tmpGateways=$(printf ",\"%s\"" "${gateways[@]}")
+					local IPS=${tmpIps:1}
+					local GATEWAYS=${tmpGateways:1}
+				fi
+			done
+
+			# Render existing template
+			render ${templateFile} > ${tempFile}
+			if (( $(grep -c '""' ${tempFile}) > 0 )); then
+				err "Not all variables are replaced"
+				# TODO: list existing or not used variables
+				return 2
+			fi
+
+			# Return commands
+			echo "'cat > template.json' < ${tempFile}"
+			echo "create -f template.json"
+			;;
+		*)
+			echo ${@}
+		;;
+	esac
+}
